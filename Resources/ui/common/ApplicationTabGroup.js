852d0fdc5df801476fadbbd77c83a17f97b4e6cf
hajipion <hajiming@gmail.com>
2013-11-17 16:20:40 +0900

    Add test

--- Resources/ui/common/ApplicationTabGroup.js
+++ Resources/ui/common/ApplicationTabGroup.js
@@ -1,22 +1,60 @@
-var view = Ti.UI.createView();
+function ApplicationTabGroup(Window) {
+	//create module instance
+	var self = Ti.UI.createTabGroup({
+		backgroundColor: "blue"
+	});
+	
+	var view = Ti.UI.createView({
+		width: 150,
+		height: 150,
+		top:180 
+	});
+	
+	// ラベルを作る
+	var label = Ti.UI.createLabel({
+		text: "Hello World" ,
+		height: 12,
+		width: 150, 
+		top: 120,
+		color: "red"
+	});
+	// ボタンをつくる
+	var button = Ti.UI.createButton({
+		title: "push me!",
+		top: 100,
+		width: 100,
+		height: 32
+	});
+	button.addEventListener("click",function(e){
+		alert("you pushed me!");
+	});
 
-var label = Ti.UI.createlabel({
-	text: "Hello",
-	height: 32,
-	width:150,
-	top:120
-});
+	//create app tabs
+	var win1 = new Window(L('home')),
+		win2 = new Window(L('settings'));
+	
+	var tab1 = Ti.UI.createTab({
+		title: L('home'),
+		icon: '/images/KS_nav_ui.png',
+		window: win1
+	});
+	win1.add(view);
+	win1.containingTab = tab1;
+	view.add(label);
+	view.add(button);
+	
+	
+	var tab2 = Ti.UI.createTab({
+		title: L('settings'),
+		icon: '/images/KS_nav_views.png',
+		window: win2
+	});
+	win2.containingTab = tab2;
+	
+	self.addTab(tab1);
+	self.addTab(tab2);
+	
+	return self;
+};
 
-var button = Ti.UI.createButton({
-	title: "push ",
-	top: 180,
-	width: 100,
-	height: 32
-});
-button.addEventListener("click", function(e){
-	alert("you pushed me!");
-});
-
-view.add(label);
-
-window.add(view);
+module.exports = ApplicationTabGroup;
