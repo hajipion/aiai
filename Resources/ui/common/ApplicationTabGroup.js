4d42a8e5ccca09f28ee0879bc3835b5e82a600e6
hajipion <hajiming@gmail.com>
2013-11-18 14:47:38 +0900

    Create tabgroup

--- Resources/ui/common/ApplicationTabGroup.js
+++ Resources/ui/common/ApplicationTabGroup.js
@@ -9,6 +9,7 @@ function ApplicationTabGroup(Window) {
 		height: 150,
 		top:180 
 	});
+	var view2 = Ti.UI.createView();
 	
 	// ラベルを作る
 	var label = Ti.UI.createLabel({
@@ -25,8 +26,17 @@ function ApplicationTabGroup(Window) {
 		width: 100,
 		height: 32
 	});
+	var alert = Ti.UI.createAlertDialog({
+		title: "delete?",
+		message: "Are you sure?",
+		buttonNames: ["OK","Cancel","Help"],
+		cancel: 1
+	});
+	alert.addEventListener("click", function(e){
+		Ti.API.info(e.index);
+	});
 	button.addEventListener("click",function(e){
-		alert("you pushed me!");
+		alert.show();
 	});
 
 	//create app tabs
@@ -40,6 +50,7 @@ function ApplicationTabGroup(Window) {
 	});
 	win1.add(view);
 	win1.containingTab = tab1;
+	win2.add(view2);
 	view.add(label);
 	view.add(button);
 	
@@ -50,6 +61,45 @@ function ApplicationTabGroup(Window) {
 		window: win2
 	});
 	win2.containingTab = tab2;
+	
+	var ebisu = Ti.Map.createAnnotation({
+		latitude: 35.645,
+		longitude: 139.71,
+		title: "Ebisu",
+		animate: true
+	});
+	var map = Ti.Map.createView({
+		mapType: Ti.Map.STANDARD_TYPE,
+		region: {latitude: 35.645, longitutde: 139.71, latitudeDelta:0.01, longitudeDelta:0.01},
+		animate: true,
+		regionFit: true,
+		width: 300,
+		height:100
+	});
+	var tf = Ti.UI.createTextField({
+		color: "#333",
+		hintText: "name",
+		height: 35,
+		top: 310,
+		left: 10,
+		width: 250,
+		borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED
+	});
+	var sbmbutton = Ti.UI.createButton({
+		title: "Submit!",
+		top:340,
+		left: 10,
+		width:100,
+		height: 40
+	});
+	sbmbutton.addEventListener("click",function(e){
+		Ti.API.info(tf.getValue());
+	});
+	view2.add(tf);
+	view2.add(sbmbutton);
+	
+	view2.add(map);
+	
 	
 	self.addTab(tab1);
 	self.addTab(tab2);
