2170442b87ff952f9e03cb79afb679fee7cf9d75
yasuko <yasuo0130@gmail.com>
2013-11-20 03:21:32 +0900

    created view3 'aiainakama' at ApplicationTabGroup.js

--- Resources/ui/common/ApplicationTabGroup.js
+++ Resources/ui/common/ApplicationTabGroup.js
@@ -19,7 +19,7 @@ function ApplicationTabGroup(Window) {
 		top:180
 	});
 	var view2 = Ti.UI.createView();
-	var view3 = Ti.UI.createView();
+	// var view3 = Ti.UI.createView();
 	var view4 = Ti.UI.createView();
 
 	//create app tabs
@@ -56,7 +56,7 @@ function ApplicationTabGroup(Window) {
 	
 	win1.add(view1);
 	win2.add(view2);
-	win3.add(view3);
+	//win3.add(view3);
 	win4.add(view4);
 	
 	// ラベルを作る
@@ -122,6 +122,103 @@ function ApplicationTabGroup(Window) {
 		Ti.API.info(tf.getValue());
 	});
 	
+	// ここからview3
+	var view3 = Ti.UI.createView({
+		backgroundImage: '/images/bg_sample.png'
+	});
+
+	// top view
+	var view3_top = Ti.UI.createView({
+		height: '35%',
+		width: 'auto',
+		fill: false,
+		top: 0
+	});
+
+	var view3_icon = Ti.UI.createView({
+		backgroundColor: '#dcdcdc',
+		opacity: 0.5,
+		height: '90%',
+		width: '80%',
+		fill: false,
+	});
+
+	var icon_image = Ti.UI.createImageView({
+		image: '/images/icon_umbrella.png'
+	});
+
+	// bottom view
+	var view3_bottom = Ti.UI.createView({
+		height: '65%',
+		width: 'auto',
+		fill: false,
+		bottom: 0
+	});
+
+	var view3_list = Ti.UI.createView({
+		height: '90%',
+		width: '80%',
+		fill: false,
+		// このなかに、リストビューを埋め込む
+		// それぞれのユーザ情報を取得して表示
+	});
+
+	var view3_list_back = Ti.UI.createView({
+		backgroundColor: '#dcdcdc',
+		opacity: 0.5
+	});
+
+	var users = [
+					{text:'user1', per: '50%'},
+					{text:'user2', per: '20%'},
+					{text:'user3', per: '70%'},
+					{text:'user4', per: '50%'},
+					{text:'user5', per: '20%'},
+					{text:'user6', per: '60%'},
+					{text:'user7', per: '10%'}
+				];//仮のデータです
+	var data = [];
+
+	for (var i=0;i<users.length;i++) {
+    	var user = users[i];
+    	var row = Ti.UI.createTableViewRow({
+    		height: 80
+    	});
+    	var nameLabel = Ti.UI.createLabel({
+    		font: { fontSize:30 },
+    		color: '#000',
+    		backgroundColor: 'blue',
+    		height: '50%',
+    		top: 0
+    	});
+    	var perLabel = Ti.UI.createLabel({
+    		font: { fontSize:25 },
+    		color: '#000',
+    		backgroundColor: 'red',
+    		height: '50%',
+    		bottom: 0
+    	});
+    	nameLabel.text = user.text;
+    	perLabel.text = user.per;
+    	row.add(nameLabel);
+    	row.add(perLabel);
+    	data.push(row);
+	}
+	var user_list = Ti.UI.createTableView({
+		data: data
+	});
+
+	view3_list.add(view3_list_back);
+	view3_list.add(user_list);
+
+	view3_top.add(view3_icon);
+	view3_top.add(icon_image);
+	view3_bottom.add(view3_list);
+
+	view3.add(view3_top);
+	view3.add(view3_bottom);
+	win3.add(view3);
+	// ここまでview3
 	
 	view1.add(label);
 	view1.add(button);
