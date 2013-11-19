30e290f5a5d454f51e68713d29474ac0beac826b
hajipion <hajiming@gmail.com>
2013-11-19 14:15:27 +0900

    setColor #2CD6B5

--- Resources/ui/common/ApplicationTabGroup.js
+++ Resources/ui/common/ApplicationTabGroup.js
@@ -1,15 +1,57 @@
 function ApplicationTabGroup(Window) {
 	//create module instance
 	var self = Ti.UI.createTabGroup({
-		backgroundColor: "#2CD6B5"
+		backgroundColor: "#2CD6B5",
+		tabsBackgroundColor : "#2CD6B5",
+		barColor: '#2CD6B5'
 	});
 	
-	var view = Ti.UI.createView({
+	
+	var view1 = Ti.UI.createView({
 		width: 150,
 		height: 150,
 		top:180
 	});
 	var view2 = Ti.UI.createView();
+	var view3 = Ti.UI.createView();
+	var view4 = Ti.UI.createView();
+
+	//create app tabs
+	var win1 = new Window(L('home')),
+		win2 = new Window(L('help')),
+		win3 = new Window(L('friends')),
+		win4 = new Window(L('setting'));
+	
+	var tab1 = Ti.UI.createTab({
+		title: L('ホーム'),
+		icon: '/images/KS_nav_ui.png',
+		window: win1
+	});
+	var tab2 = Ti.UI.createTab({
+		title: L('ヘルプ'),
+		icon: '/images/KS_nav_views.png',
+		window: win2
+	});
+	var tab3 = Ti.UI.createTab({
+		title: L('相合仲間'),
+		icon: '/images/KS_nav_views.png',
+		window: win3
+	});
+	var tab4 = Ti.UI.createTab({
+		title: L('設定'),
+		icon: '/images/KS_nav_views.png',
+		window: win4
+	});
+	
+	win1.containingTab = tab1;
+	win2.containingTab = tab2;
+	win3.containingTab = tab3;
+	win4.containingTab = tab4;
+	
+	win1.add(view1);
+	win2.add(view2);
+	win3.add(view3);
+	win4.add(view4);
 	
 	// ラベルを作る
 	var label = Ti.UI.createLabel({
@@ -38,30 +80,8 @@ function ApplicationTabGroup(Window) {
 	button.addEventListener("click",function(e){
 		alert.show();
 	});
-
-	//create app tabs
-	var win1 = new Window(L('home')),
-		win2 = new Window(L('settings'));
 	
-	var tab1 = Ti.UI.createTab({
-		title: L('home'),
-		icon: '/images/KS_nav_ui.png',
-		window: win1
-	});
-	win1.add(view);
-	win1.containingTab = tab1;
-	win2.add(view2);
-	view.add(label);
-	view.add(button);
-	
-	
-	var tab2 = Ti.UI.createTab({
-		title: L('settings'),
-		icon: '/images/KS_nav_views.png',
-		window: win2
-	});
-	win2.containingTab = tab2;
-	
+	//　マップためしてみた
 	var ebisu = Ti.Map.createAnnotation({
 		latitude: 35.645,
 		longitude: 139.71,
@@ -95,14 +115,21 @@ function ApplicationTabGroup(Window) {
 	sbmbutton.addEventListener("click",function(e){
 		Ti.API.info(tf.getValue());
 	});
+	
+	
+	view1.add(label);
+	view1.add(button);
+	
 	view2.add(tf);
 	view2.add(sbmbutton);
-	
 	view2.add(map);
 	
+
 	
 	self.addTab(tab1);
 	self.addTab(tab2);
+	self.addTab(tab3);
+	self.addTab(tab4);
 	
 	return self;
 };
