85427574144d7f2b5e598b70a85c70689ae3b737
yasuko <yasuo0130@gmail.com>
2013-11-23 21:23:16 +0900

    no message

--- build/iphone/build/aiai.build/Debug-iphonesimulator/aiai.build/Objects-normal/i386/LauncherView.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/LauncherView.m \
-  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/LauncherView.h \
-  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/LauncherItem.h \
-  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/LauncherButton.h \
-  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/TiBase.h \
-  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/TiThreading.h \
-  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/TiPublicAPI.h
--- build/iphone/build/aiai.build/Debug-iphonesimulator/aiai.build/Objects-normal/i386/TiComplexValue.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/TiComplexValue.m \
-  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/TiComplexValue.h \
-  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/TiBase.h \
-  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/TiThreading.h \
-  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/TiPublicAPI.h
