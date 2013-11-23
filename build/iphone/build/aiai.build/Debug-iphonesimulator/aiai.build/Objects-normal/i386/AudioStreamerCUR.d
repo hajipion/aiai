ded1a9e06ece56b1ea4c259ea5a3aa5775373c6a
hajipion <hajiming@gmail.com>
2013-11-23 20:15:33 +0900

    Change Default.png for Android All Devices & Environment iPhone

--- build/iphone/build/aiai.build/Debug-iphonesimulator/aiai.build/Objects-normal/i386/AudioStreamerCUR.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/AudioStreamer/AudioStreamerCUR.m \
-  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/AudioStreamer/AudioStreamerCUR.h \
-  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/AudioStreamer/AudioStreamer.h
