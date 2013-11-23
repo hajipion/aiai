aeffd5d8e3a5cb996bf1efe4254ec3285fa9e05d
hajipion <hajiming@gmail.com>
2013-11-24 01:15:40 +0900

    Create profile.js

--- Resources/ui/common/profile.js
+++ Resources/ui/common/profile.js
@@ -5,11 +5,9 @@ function ProfileWindow(parent){
 		title: 'Profile'
 	});
 	*/
+	
 	// base view 
 	var view = Ti.UI.createView({
-		layout: 'vertical',
-		height: 'auto',
-		width: 'auto',
 		backgroundImage: '/images/bg_sample.png'
 	});
 
@@ -21,68 +19,86 @@ function ProfileWindow(parent){
 		top: 0
 	});
 
-	var view_top_back = Ti.UI.createView({
-		backgroundColor: '#dcdcdc',
-		opacity: 0.5,
-		height: '90%',
-		width: '80%',
-		fill: false,
-	});
-	
 	var icon_image = Ti.UI.createImageView({
-		image: '/images/icon_umbrella.png'
+		image: '/images/icon_umbrella.png',
+		width: 130,
+		top:10
 	});
 
 	// bottom view
 	var view_bottom = Ti.UI.createView({
-		height: '65%',
+		height: '83%',
 		width: 'auto',
 	  	fill: false,
+	  	top: '22%',
 		bottom: 0
 	});
 	
 	var view_bottom_back = Ti.UI.createView({
 	 	backgroundColor: '#dcdcdc',
-	 	opacity: 0.5
+	 	opacity: 0.7,
+	 	top:60,
+	 	width: '80%',
+		height: '70%'
 	});
 
 	var view_prof = Ti.UI.createView({
-		height: '60%',
-		width: '80%',
+		width: '100%',
 		fill: false,
 		top: 0
-		// このなかに、リストビューを埋め込む
-		// それぞれのユーザ情報を取得して表示
 	});
 
 	var prof_kind = [ 
-						{text:'相合仲間', num: '24人'},
-						{text:'助けた回数', num: '2回'},
-						{text:'助けてもらった回数', num: '2回'},
-						{text:'紳士度', num: '10%'}
+						{text:'相合仲間 ', num: '24人'},
+						{text:'助けた回数 ', num: '2回'},
+						{text:'助けてもらった回数 ', num: '2回'},
+						{text:'紳士度 ', num: '10%'}
 					];
-	
+
 	var nameLabel = Ti.UI.createLabel({
 		text: '山田 太郎',
-		height: '25%',
-		top: 0
+		top: 20,
+    	font: { fontSize:23 },
+	    color: '#000',
+	    fontWeight: 'bold'
 	});
 	view_prof.add(nameLabel);
 	var view_profLabel = Ti.UI.createView({
 		height: '75%',
+		top: 65,
 		bottom: 0,
 		layout: 'vertical'
 	});
-	
+
 	for(var i=0;i<prof_kind.length;i++){
-		var label = Ti.UI.createLabel({
-			top: 0,
-			height: '25%'
+		var profLabel = Ti.UI.createLabel({
+			left: 20,
+			width: '65%',
+			color: '#666'
 		});
-		label.text = prof_kind[i].text;
-		view_profLabel.add(label);
+		var numLabel = Ti.UI.createLabel({
+			left: 15,
+			width: '20%',
+			color: '#38695A',
+			font: {fontSize: 20}
+		});
+		var view_profList = Ti.UI.createView({
+			width: '100%',
+			height: '21%',
+			bottom: 0,
+			layout: 'horizontal'
+		});
+		profLabel.text = prof_kind[i].text;
+		numLabel.text = prof_kind[i].num;
+		view_profList.add(profLabel);
+		view_profList.add(numLabel);
+		view_profLabel.add(view_profList);
 	}
-	
+	view_prof.add(view_profLabel);
+	view_top.add(icon_image);
+	view_bottom_back.add(view_prof);
+	view_bottom.add(view_bottom_back);
+
 	var back_btn = Ti.UI.createButton({
 		title: "back",
 		width: '80%',
@@ -92,17 +108,12 @@ function ProfileWindow(parent){
 	back_btn.addEventListener('click', function(e){
 		view.visible = false;
 	});
-	view_prof.add(view_profLabel);
-	view_prof.add(view_bottom_back);
-	view_top.add(view_top_back);
-	view_top.add(icon_image);
-	view_bottom.add(view_prof);
+	
 	view_bottom.add(back_btn);
 
 	view.add(view_top);
 	view.add(view_bottom);
 	
-
 	//win.add(view);
 
 	return view;
