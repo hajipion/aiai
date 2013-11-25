cb4ee6e0b77849dd7bdd09188072f0af6b9c981a
yoko <yoko.ichioka@is.ocha.ac.jp>
2013-11-25 21:10:57 +0900

    写真投稿（なんかさっき間違えたcomitしたかも） ユーザはすでに写真を投稿してたら，写真を表示，なかったら，アップロードさせる（実機で要確認！）todo：投稿ボタンで投稿させる．投稿した後，再度取得をするように

--- Resources/ui/common/window4.js
+++ Resources/ui/common/window4.js
@@ -14,13 +14,14 @@ function Window4(title){
 		{title:'ログアウト', hasSwitch: false}
 	];
 	
-	//画像取得用api
+	//画像取得用api(ログイン必須)一番最新の写真が表示
 	 var Cloud = require('ti.cloud');
     Cloud.debug = true;
 	
 	Cloud.Users.showMe(function (e) {
     if (e.success) {
         var user = e.users[0];
+        if(user.photo){//ユーザが写真はあったら表示
           var anImageView = Ti.UI.createImageView({
                     image : user.photo.urls.thumb_100,
                     width : 100,
@@ -30,11 +31,38 @@ function Window4(title){
                 });
                 //適当に表示
                 win.add(anImageView);
+        }else{
+        		//写真投稿はこちらログインしてないとエラー（実機でためさないと・・・）
+			 Ti.Media.openPhotoGallery({
+             success : function(event) {
+                Cloud.Photos.create({
+                    photo: event.media
+                }, function (e) {
+                    if (e.success) {
+                        var photo = e.photos[0];
+                        alert('Success:\\n' +
+                            'id: ' + photo.id + '\\n' +
+                            'filename: ' + photo.filename + '\\n' +
+                            'size: ' + photo.size,
+                            'updated_at: ' + photo.updated_at);
+                    } else {
+                        alert('Error:\\n' +
+                            ((e.error && e.message) || JSON.stringify(e)));
+                    }
+                });
+            },
+            mediaTypes : [Ti.Media.MEDIA_TYPE_PHOTO]
+       	 });
+		//写真投稿ここまで
+        }
+        
     } else {
         alert('Error:\n' +
             ((e.error && e.message) || JSON.stringify(e)));
     }
 });
+
+	
 
 	var data = [];
 
