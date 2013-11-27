10227ce9a3429eee29375e423b5c3b335d4ebe5f
yoko <yoko.ichioka@is.ocha.ac.jp>
2013-11-28 01:24:02 +0900

    とりあえず，起動したら，yokoユーザではいります．かってにtabgroupに飛びます

--- Resources/app.js
+++ Resources/app.js
@@ -35,7 +35,88 @@ if (Ti.version < 1.8 ) {
 	else {
 		Window = require('ui/handheld/ApplicationWindow');
 	}
+	
+　　 var name = Ti.App.Properties.getString('username');
+    var pass = Ti.App.Properties.getString('pass');
 
-	var ApplicationTabGroup = require('ui/common/login');
-	new ApplicationTabGroup(Window).open();
+	 if (name) {
+            alert(name);
+            login_user()
+            var ApplicationTabGroup =require('ui/common/ApplicationTabGroup');
+           
+           
+        }else{
+        	var ApplicationTabGroup = require('ui/common/login');
+        	new ApplicationTabGroup(Window).open();
+        }
+
+  function login_user(){
+	
+	var Cloud = require('ti.cloud');
+    Cloud.debug = true;
+  
+    //友達変数
+ 	Ti.App._withFriends =[];
+    // ユーザー作成API呼び出し（一回ログインしたら再度ログインしなくていいようにしないとね）
+    //button.title = 'ログイン';
+    //button.addEventListener('click', function(e) {
+   	var ApplicationTabGroup =require('ui/common/ApplicationTabGroup');
+
+       // username = userNameText.value;
+     
+        //usernameを保存
+   	Ti.App._username = "yoko";
+       // pass = passText.value;
+        
+    //var loginname = Ti.App.Properties.getString('username');
+    //var loginpass = Ti.App.Properties.getString('pass');
+	Ti.App.Properties.setString('username',  name);
+        Cloud.Users.login({
+        login:    "yoko",
+        password: "yokoyoko"
+   			 }, function (e) {
+        if (e.success) {
+            var user = e.users[0];
+            //user_idを保存
+            Ti.App._userid =user.id;
+            alert('Success:\\n' +
+                'id: ' + user.id + '\\n' +
+                'first name: ' + user.first_name + '\\n' +
+                'last name: ' + user.last_name);
+   		//ともだち取得(ちょっと保留)
+
+   		Cloud.debug = true;
+		Cloud.Friends.search({
+		user_id: Ti.App._userid
+		}, function (e) {
+    	if (e.success&&e.users.length>0) {
+
+        for (var i = 0; i < e.users.length; i++) {
+          		var user = e.users[i];
+
+		  		var currentWithFriends = Ti.App._withFriends;
+	     		 var ids = {
+   					text:user.username,
+   					pt:'50pt',
+				};
+			currentWithFriends.push(ids);
+	      	Ti.App._withFriends=currentWithFriends;
+		  //友達情報を読み込んでからページをopen！！
+		  	new ApplicationTabGroup(Window).open();
+        	 }
+     }else{
+     		new ApplicationTabGroup(Window).open();
+    	}
+	});
+ } else {
+            alert('ログインできないでーすユーザくつくりまーす！:\\n' +
+                ((e.error && e.message) || JSON.stringify(e)));
+    
+        }
+   	});
+   // });
+
+
+}	
+	
 })();
