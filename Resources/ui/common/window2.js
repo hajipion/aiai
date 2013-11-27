0bf4948b18a38a3f7789a3e151a96506dca78b48
yasuko <yasuo0130@gmail.com>
2013-11-28 03:52:23 +0900

    レイアウトと位置情報送信のメソッド追加したけど、(適当 そもそもalertのokボタン押すとエラーで終わる。websocket is undefinedとかいうメッセージとともに

--- Resources/ui/common/window2.js
+++ Resources/ui/common/window2.js
@@ -1,10 +1,18 @@
 function Window2(title){
+	
+	/*
+	 * ヘルプボタンが押されたらユーザに通知
+	 * 他ユーザから助けるよ通知もらったらソケット通信開始
+	 * ソケット通信でJSONが送られてきたらユーザ全員の位置を表示
+	 * 完了ボタン(未)押したら通信終了。全員に通知。
+	 * あとは？
+	 */
 
 	var win = Ti.UI.createWindow({
 		title: title
 	});
 
-	// base view
+	// View ////////////////////////////////////////////////////////////
 	var view = Ti.UI.createView({
 		// layout: 'vertical'
 	});
@@ -24,7 +32,7 @@ function Window2(title){
 		color: "#333",
 		hintText: "現在地",
 		width: '60%',
-		height: '30%',
+		height: '25%',
 		top: 10,
 		editable: false,
 		borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED
@@ -35,7 +43,7 @@ function Window2(title){
 		color: "#333",
 		hintText: "目的地を入力",
 		width: '60%',
-		height: '30%',
+		height: '25%',
 		top: 10,
 		borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED
 	});
@@ -61,8 +69,8 @@ function Window2(title){
 	
 	// 閉じるボタン
 	var closeBtn = Ti.UI.createButton({
-		width: 55,
-		height: 55,
+		width: 30,
+		height: 30,
 		right: 10,
 		backgroundImage: '/images/close.png',
 		opacity:1
@@ -76,7 +84,7 @@ function Window2(title){
 	
 	var view_buttons = Ti.UI.createView({
 		top: 10,
-		height: '30%',
+		height: '25%',
 		width: '100%'
 	});
 	view_buttons.add(sbmbutton);
@@ -103,6 +111,21 @@ function Window2(title){
 		view_search.visible = true;
 	});
 	
+	// map
+	var mapview = Ti.Map.createView({
+ 		mapType: Ti.Map.STANDARD_TYPE,
+ 		region: {latitude:35.681382, longitude:139.76608, latitudeDelta:0.01, longitudeDelta:0.01},
+ 		animate: true,
+ 		regionFit: true,
+ 		width: 'auto',
+ 		height: 'auto'
+	});
+	
+	view.add(mapview);
+	view.add(view_search);
+	view.add(pullBtn);
+	win.add(view);
+	
 	// アラート
 	var alert = Ti.UI.createAlertDialog({
 		//title: "HELP?",
@@ -127,40 +150,18 @@ function Window2(title){
 	    }
 	});
 
-
-	// 最初に中心となる位置をセットしておく
-	var mapview = Ti.Map.createView({
- 		mapType: Ti.Map.STANDARD_TYPE,
- 		region: {latitude:35.681382, longitude:139.76608, latitudeDelta:0.01, longitudeDelta:0.01},
- 		animate: true,
- 		regionFit: true,
- 		width: 'auto',
- 		height: 'auto'
-	});
-
-	view.add(mapview);
-	view.add(view_search);
-	view.add(pullBtn);
-	win.add(view);
-
+	// use geolocation ////////////////////////////////////////////////////
+	// check locationService 
 	if(!Titanium.Geolocation.locationServicesEnabled){
         var alt = Titanium.UI.createAlertDialog({
             title:'位置情報取得',
             message:'位置測定が出来ません。電波状況、設定を確認してください。'
         });
-        //alt.show();
+        alt.show();
         return;
     }
-
-    /*
-	// 現在位置
-	var my_place = Ti.Map.createAnnotation({
-		title: "現在地",
-		pinImage: "/images/map-pin.png",
-		animate: true
-	});
-	*/
-
+	
+	// setting
 	Ti.Geolocation.purpose = 'Get Current Location';
     Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_BEST;
     Ti.Geolocation.distanceFilter = 5;
@@ -170,11 +171,18 @@ function Window2(title){
 		var providerGps = Ti.Geolocation.Android.createLocationProvider({
 		    name: Ti.Geolocation.PROVIDER_GPS,
 	    	minUpdateDistance: 5.0,
-	    	minUpdateTime: 10
+	    	minUpdateTime: 5
 		});
 		Ti.Geolocation.Android.addLocationProvider(providerGps);
 		Ti.Geolocation.Android.manualMode = true;
 	}
+	
+	// ユーザの位置情報オブジェクトを作成
+	var user_place = {
+		name: Ti.App._user,
+		latitude: 0.0,
+		longtitude: 0.0
+	};
 	
 	// 現在地を動的に表示する
   	var currentPos = Titanium.Map.createAnnotation({
@@ -183,6 +191,7 @@ function Window2(title){
    		animate: true
   	});
 		
+	// event 'location' callback this function 
 	var locationCallback = function(e) {
     	if(!e.success || e.error){
    				//alert('位置情報が取得できませんでした');
@@ -192,14 +201,15 @@ function Window2(title){
     	var latitude = e.coords.latitude;
     	var longitude = e.coords.longitude;
 
+		user_place.latitude = latitude;
+		user_place.longitude = longitude;
+		//sendLocation();
     	// 小数点第二位に省略
-        var shortLatitude = Math.round(latitude * 100) / 100;
-        var shortLongitude = Math.round(longitude * 100) / 100;
+        var shortLatitude = Math.round(user_place.latitude * 100) / 100;
+        var shortLongitude = Math.round(user_place.longitude * 100) / 100;
 
-        // テキストフィールドに現在地を書く
-        // tfPresent.setValue('現在地：'+shortLatitude+','+shortLongitude);
-		tfPresent.setValue(''+latitude);
-		tfDestination.setValue(''+longitude);
+		tfPresent.setValue(''+user_place.latitude);
+		tfDestination.setValue(''+user_place.longitude);
 		currentPos.latitude=latitude;
 		currentPos.longitude=longitude;
 	  		
@@ -213,9 +223,22 @@ function Window2(title){
             longitudeDelta:0.01
         });
 	};
+	
+	// 位置情報を送信
+	var sendLocation = function(){
+		var io = require('ui/common/socketio-titanium');
+			var socket = io.connect('202.181.102.188:8080');
+			
+			socket.emit("message", JSON.stringify(user_place));
+			// event handler (recieve)
+			socket.on("message", function (data){
+				// たぶんここでJSON受け取ってみんなの位置を表示せなあかんのかな？(sendlocationの中じゃないだろうけど)
+			});
+	};
 
 	Titanium.Geolocation.addEventListener('location', locationCallback);
 
+	
 	/*　住所から緯度経度
 	Ti.Geolocation.forwardGeocoder('440 Bernardo Ave Mountain View CA', function(e) {
    			latitude = e.latitude;
