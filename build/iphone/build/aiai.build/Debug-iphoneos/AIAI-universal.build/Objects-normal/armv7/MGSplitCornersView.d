fbd3b4212c0065c1b6c8cd13e93f78e99ec74464
hajipion <hajiming@gmail.com>
2013-11-27 19:43:08 +0900

    Env

---
+++ build/iphone/build/aiai.build/Debug-iphoneos/AIAI-universal.build/Objects-normal/armv7/MGSplitCornersView.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/MGSplitView/MGSplitCornersView.m \
+  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/TiBase.h \
+  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/TiThreading.h \
+  /Users/hajime/Documents/Titanium_Studio_Workspace/aiai/build/iphone/Classes/TiPublicAPI.h
